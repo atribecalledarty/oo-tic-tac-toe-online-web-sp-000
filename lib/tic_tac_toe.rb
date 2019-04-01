b0fd5e7eba8f8dd909fb86f4dacb861a18dcb3ef
Arthur Song <arth3rs0ng@gmail.com>
2019-04-01 19:02:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -30,10 +30,10 @@ class TicTacToe
     @board[index] = char
   end
   
-  def position_taken?(board, index)
-    if board[index] == " " || board[index] == nil
+  def position_taken?(index)
+    if @board[index] == " " || @board[index] == nil
       return false
-    elsif board[index] == "X" || board[index] == "O"
+    elsif @board[index] == "X" || @board[index] == "O"
       return true
     end
   end
